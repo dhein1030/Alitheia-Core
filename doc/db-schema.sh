5ed92140774322281ee0513f8005ed0ad17e5e11
Dan Hein <dhein@crunchbang.(none)>
2014-06-04 20:26:43 -0500

    Fix capitalization in db-schema.sh
    Changed UMLGraph.jar to UmlGraph.jar

--- doc/db-schema.sh
+++ doc/db-schema.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
-#To produce: place UMLGraph.jar in this directory and run
+#To produce: place UmlGraph.jar in this directory and run
 #
 
-if [ ! -e UMLGraph.jar ]; then
+if [ ! -e UmlGraph.jar ]; then
     curl -o UmlGraph.jar http://search.maven.org/remotecontent?filepath=org/umlgraph/umlgraph/5.6/umlgraph-5.6.jar
-    #echo Copy UMLGraph.jar to `pwd` and re-run
+    #echo Copy UmlGraph.jar to `pwd` and re-run
     #exit 1
 fi
 
 cwd=`pwd`
-cp UMLGraph.jar ../alitheia/core/src/main/java/eu/sqooss/service/db
+cp UmlGraph.jar ../alitheia/core/src/main/java/eu/sqooss/service/db
 cd ../alitheia/core/src/main/java/eu/sqooss/service/db
 java -jar UmlGraph.jar -attributes -private -hide "Ohloh|DAObject|FileGroup|ProjectVersionParentId|DBService|StoredProjectConfig" *.java
 cat graph.dot |grep -v STATE|grep -v MASK|grep -v SCM_ROOT|grep -v DEFAULT >graph1.dot
